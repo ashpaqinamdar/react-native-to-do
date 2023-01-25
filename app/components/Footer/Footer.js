eced1ec4dd7ae43e9ac4d51ff258b4c5a673458b
Ashpaq <ashpaq.inamdar@mindbowser.com>
2023-01-25 12:43:50 +0530

    initial commit

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Linking } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Footer = () => (
+  <View
+    style={{
+      paddingHorizontal: 40,
+      paddingVertical: 25,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#323742",
+    }}
+  ></View>
+);
+
+export default Footer;
